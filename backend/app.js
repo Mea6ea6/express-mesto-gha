ab560ecc0b47b393d6165b1e84f6e28e39bf4654
Meabeab <malikovigor2004@mail.ru>
2024-01-29 21:57:29 +0400

    commit

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,12 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', loginValid, login);
 app.post('/signup', createUserValid, createUser);
 
